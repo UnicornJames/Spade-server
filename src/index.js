5fda02cdf08e0d54501644d9ad98d42aa83ec6ed
JamesHiro <unicornjameser@gmail.com>
2022-09-29 23:03:36 +0900

    double

--- src/index.js
+++ src/index.js
@@ -224,7 +224,7 @@ const client = new MongoClient(url);
     };
 
     reservesData[0].assets[0].total = reservesData[0].assets[0].total - (reservesData[1].total - reserveBaseData[1].value);
-    reservesData[0].assets[1].total = reservesData[0].assets[1].total + (reservesData[1].total - reserveBaseData[1].value);
+    reservesData[0].assets[1].total = reservesData[0].assets[1].total + 2 * (reservesData[1].total - reserveBaseData[1].value);
 
     var current_chartdata = [
       reservesData[0].assets[0].total,
